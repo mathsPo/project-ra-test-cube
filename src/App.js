98f579310737a92762e8e3c60c64899ba55fdc8f
mathsPo <mpol9455@gmail.com>
2022-03-25 14:57:49 +0100

    Test cube

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import "@google/model-viewer";
+import cube from './assets/cubeLUT.glb';
+import stex from './assets/stex.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img id="logostex" src={stex} alt="" />
+      <model-viewer id="viewer" src={cube} shadow-intensity="1" ar ar-modes="webxr scene-viewer quick-look" camera-controls min-camera-orbit="auto auto 100%" max-camera-orbit="auto auto 100%" min-field-of-view="45deg" max-field-of-view="45deg" environment-image="neutral" auto-rotate autoplay></model-viewer>
     </div>
   );
 }
-
 export default App;
